358b78680833e4796654faa3f298815cfe8a82b3
Alex Carter <victoryforphil@gmail.com>
2026-03-05 01:29:02 -0800

    GreenField

--- datastore/datastore-core/prisma/migrations/20260305032740_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "text_messages" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "user" TEXT NOT NULL,
-    "platform" TEXT NOT NULL,
-    "message" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
